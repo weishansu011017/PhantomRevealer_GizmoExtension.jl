c7f7e6dcfce12acce1003cbb604f674dfeff9467
Wei-Shan Su <weishansu011017@gmail.com>
2025-03-31 00:05:18 +0800

    Update 0.1.1
    - add two function to calculate sound speed and Pressure.

--- src/julia/add_quantities.jl
+++ src/julia/add_quantities.jl
@@ -43,7 +43,7 @@ end
 
 """
     add_NumberDensityGas!(data::PhantomRevealerDataFrame)
-Add the number of density of gas particles for each particles. Valid only if `data` with `data.params["PartType"] == "PartType0` (Gas particles) is provided.
+Add the number of density of monatomic gas particles for each particles. Valid only if `data` with `data.params["PartType"] == "PartType0` (Gas particles) is provided.
 
 # Parameters
 - `data :: PhantomRevealerDataFrame`: The SPH data that is stored in `PhantomRevealerDataFrame` 
@@ -53,6 +53,43 @@ function add_NumberDensityGas!(data::PhantomRevealerDataFrame)
         umass = data.params["umass"]
         mp = 1.6e-24/umass
         data[!,"n"] = (data[!,"rho"] ./mp)
+    else
+        nothing
+    end
+end
+
+"""
+    add_Pressure_adiabetic!(data::PhantomRevealerDataFrame; adiabatic_index::Float64 = 1.6666666667)
+Add the pressure of gas particles for each particles. Valid only if `data` with `data.params["PartType"] == "PartType0` (Gas particles) is provided.
+
+# Parameters
+- `data :: PhantomRevealerDataFrame`: The SPH data that is stored in `PhantomRevealerDataFrame` 
+
+# Keyword arguments
+- `adiabatic_index :: Float64 = 1.6666666667`: The adiabatic index of system. Default to be non-relativistic monatomic gas(γ = 5/3).
+"""
+function add_Pressure_adiabetic!(data::PhantomRevealerDataFrame; adiabatic_index::Float64 = 1.6666666667)
+    if (data.params["PartType"] == "PartType0")
+        data[!,"P"] = (adiabatic_index - 1) .* data[!,"rho"] .* data[!,"InternalEnergy"] * 1.0004250399845307   # 10^10 M⊙ kpc^-3 (km/s)^2  => 10^10 M⊙ kpc^-1 Gyr^-2
+    else
+        nothing
+    end
+end
+
+"""
+    add_Soundspeed_adiabetic!(data::PhantomRevealerDataFrame; adiabatic_index::Float64 = 1.6666666667)
+Add the sound of speed of gas particles for each particles. Valid only if `data` with `data.params["PartType"] == "PartType0` (Gas particles) is provided.
+
+# Parameters
+- `data :: PhantomRevealerDataFrame`: The SPH data that is stored in `PhantomRevealerDataFrame` 
+
+# Keyword arguments
+- `adiabatic_index :: Float64 = 1.6666666667`: The adiabatic index of system. Default to be non-relativistic monatomic gas(γ = 5/3).
+"""
+function add_Soundspeed_adiabetic!(data::PhantomRevealerDataFrame; adiabatic_index::Float64 = 1.6666666667)
+    if (data.params["PartType"] == "PartType0")
+        cons = adiabatic_index*(adiabatic_index - 1)
+        data[!,"cs"] = sqrt.(cons .* data[!,"InternalEnergy"])  # In the unit of km/s
     else
         nothing
     end
